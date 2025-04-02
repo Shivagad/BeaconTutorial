d272f225a5b564dae4e8e7c0964bdf6a7fdfeccf
Shivaji Gadekar <gadekarshivaji8767@gmail.com>
2025-04-02 22:07:25 +0530

    mongodburl

--- Backend/index.js
+++ Backend/index.js
@@ -74,9 +74,9 @@ app.get("/s", (req, res) => {
 });
 
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
 
 
 
